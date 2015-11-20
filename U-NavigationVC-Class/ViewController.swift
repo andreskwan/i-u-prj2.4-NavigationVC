f13126a59cfc2b8da5460c3f8e491bc842617089
ANDRES KWAN <andreskwan@gmail.com>
2015-11-20 05:54:56 -0500

    Initial Commit

---
+++ U-NavigationVC-Class/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  U-NavigationVC-Class
+//
+//  Created by Andres Kwan on 11/20/15.
+//  Copyright © 2015 Kwan-udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
